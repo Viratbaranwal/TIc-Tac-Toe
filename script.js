bc06d7dfafd70eef782bfb2ef37426c9079c9818
Virat Raj <viratraj@Virats-MacBook-Air.local>
2023-03-14 10:28:10 +0530

    Edited

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let tap = new Audio("tap.mp3");
 let gameover = new Audio("gameover.mp3");
 let isgameover = false;
 let turn = "X";
+let Player1 = prompt("Enter 1st Player Name: ");
+let Player2 = prompt("Enter 2nd Player Name: ");
+var count = 0;
 
 const changeTurn = ()=> {
     return turn === "X" ? "0" : "X";
@@ -23,16 +26,23 @@ const checkWin = ()=> {
     ]
     wins.forEach(e =>{
         if((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[0]].innerText !== "")){
-            document.querySelector('.info').innerText = boxtexts[e[0]].innerText + " Won";
+            if(boxtexts[e[0]].innerText === "X"){
+            document.querySelector('.info').innerText = Player1 + " Won";
+            }
+            else if(boxtexts[e[0]].innerText === "0"){
+                document.querySelector('.info').innerText = Player2 + " Won";
+            }
+            gameover.play();
             isgameover = true;
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
         }
     })
 }
-
-
-let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element =>{
+if(isgameover===false){
+    muisc.play();
+}
+    let boxes = document.getElementsByClassName("box");
+    Array.from(boxes).forEach(element =>{
     let boxtext = element.querySelector('.boxtext');
     element.addEventListener('click', ()=>{
         if(boxtext.innerText === ''){
@@ -40,7 +50,12 @@ Array.from(boxes).forEach(element =>{
             turn = changeTurn();
             tap.play();
             checkWin();
-            if(!isgameover){
+            count++;
+            if(count === 9 && isgameover === false)
+            {
+                document.getElementsByClassName("info")[0].innerText = " Oops.. Nobody Won!! ";
+            }
+            else if(!isgameover){
                 document.getElementsByClassName("info")[0].innerText = "Turn For " + turn;
             } 
         }
@@ -57,7 +72,13 @@ reset.addEventListener('click', ()=>{
     isgameover = false;
     document.getElementsByClassName("info")[0].innerText = "Turn For " + turn;
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
+    Player1 = prompt("Enter 1st Player Name: ");
+    Player2 = prompt("Enter 2nd Player Name: ");
 
 })
+
+
+
+
 
 
